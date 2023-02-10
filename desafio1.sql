466d0b4583b3ad8ff1b1c371e45fb7a26633eb7e
AndreHammel <hammel.engineer@gmail.com>
2023-02-10 13:16:16 -0300

    feat: desafio 1 concluido - tabela normalizada e povoada

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,141 @@
 DROP DATABASE IF EXISTS SpotifyClone;
 
-CREATE DATABASE IF NOT EXISTS SpotifyClone;
+CREATE DATABASE SpotifyClone;
 
+CREATE TABLE SpotifyClone.Plans (
+  id_plan INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  plan_name VARCHAR(100) NOT NULL,
+  plan_value DECIMAL(5, 2)
+);
+
+INSERT INTO SpotifyClone.Plans (plan_name, plan_value) 
+VALUES 	("gratuito", 0.00),
+        ("familiar", 7.99),
+        ("universitário",	5.99),
+        ("pessoal",	6.99);
+
+CREATE TABLE SpotifyClone.Users (
+  id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_name VARCHAR(200) NOT NULL,
+  age INT,
+  id_plan INT NOT NULL,
+  assignment_plan DATE NOT NULL,
+  FOREIGN KEY (id_plan) REFERENCES Plans(id_plan)
+);
+
+INSERT INTO SpotifyClone.Users (user_name, age, id_plan, assignment_plan)
+VALUES 
+("Barbara Liskov", 82,	1, "2019-10-20"),
+("Robert Cecil Martin",	58,	1,	"2017-01-06"),
+("Ada Lovelace",	37,	2,	"2017-12-30"),
+("Martin Fowler",	46,	2,	"2017-01-17"),
+("Sandi Metz",	58,	2,	"2018-04-29"),
+("Paulo Freire",	19,	3,	"2018-02-14"),
+("Bell Hooks",	26,	3,	"2018-01-05"),
+("Christopher Alexander",	85,	4,	"2019-06-05"),
+("Judith Butler",	45,	4,	"2020-05-13"),
+("Jorge Amado",	58,	4,	"2017-02-17");
+
+CREATE TABLE SpotifyClone.Artists (
+  id_artist INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  artist_name VARCHAR(200)
+);
+
+INSERT INTO SpotifyClone.Artists (artist_name)
+VALUES  ("Beyoncé"),
+        ("Queen"),
+        ("Elis Regina"),
+        ("Baco Exu do Blues"),
+        ("Blind Guardian"),
+        ("Nina Simone");
+
+CREATE TABLE SpotifyClone.User_following_artists (
+  id_user INT NOT NULL,
+  id_artist INT NOT NULL,
+  CONSTRAINT PRIMARY KEY (id_user, id_artist),
+  FOREIGN KEY (id_user) REFERENCES Users(id_user),
+  FOREIGN KEY (id_artist) REFERENCES Artists(id_artist)
+);
+
+INSERT INTO SpotifyClone.User_following_artists (id_user, id_artist)
+VALUES  (1,	1),
+        (1,	2),
+        (1,	3),
+        (2,	1),
+        (2,	3),
+        (3,	2),
+        (4,	4),
+        (5,	5),
+        (5,	6),
+        (6,	6),
+        (6,	1),
+        (7,	6),
+        (9,	3),
+        (10, 2);
+
+CREATE TABLE SpotifyClone.Albuns (
+  id_albun INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  albun_name VARCHAR(200),
+  id_artist INT NOT NULL,
+  release_year INT,
+  FOREIGN KEY (id_artist) REFERENCES Artists(id_artist)
+);
+
+INSERT INTO SpotifyClone.Albuns (albun_name, id_artist, release_year)
+VALUES  ("Renaissance",	1,	2022),
+        ("Jazz", 2,	1978),
+        ("Hot Space", 2, 1982),
+        ("Falso Brilhante", 3, 1998), 
+        ("Vento de Maio", 3, 2001),
+        ("QVVJFA?",	4, 2003), 
+        ("Somewhere Far Beyond", 5 , 2007),
+        ("I Put A Spell On You", 6, 2012);
+
+CREATE TABLE SpotifyClone.Songs (
+  id_song INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  song_name VARCHAR(300) NOT NULL,
+  id_albun INT NOT NULL,
+  duration INT NOT NULL,
+  FOREIGN KEY (id_albun) REFERENCES Albuns(id_albun)
+);
+
+INSERT INTO SpotifyClone.Songs (song_name, id_albun, duration)
+VALUES  ("BREAK MY SOUL", 1, 279),
+        ("VIRGO’S GROOVE", 1, 369),
+        ("ALIEN SUPERSTAR", 1, 116),
+        ("Don’t Stop Me Now", 2, 203),
+        ("Under Pressure", 3, 152),
+        ("Como Nossos Pais", 4, 105),
+        ("O Medo de Amar é o Medo de Ser Livre" ,5 ,207),
+        ("Samba em Paris" ,6 ,267),
+        ("The Bard’s Song" ,7	,244),
+        ("Feeling Good"	,8 ,100);
+
+
+CREATE TABLE SpotifyClone.History_users_songs (
+  id_user INT NOT NULL,
+  id_song INT NOT NULL,
+  last_reproduction DATETIME,
+  CONSTRAINT PRIMARY KEY (id_user, id_song),
+  FOREIGN KEY (id_user) REFERENCES Users(id_user),
+  FOREIGN KEY (id_song) REFERENCES Songs(id_song)
+);
+
+INSERT INTO SpotifyClone.History_users_songs (id_user, id_song, last_reproduction)
+VALUES
+(1 ,8	,"2022-02-28 10:45:55"),
+(1 ,2	,"2020-05-02 05:30:35"),
+(1 ,10 ,"2020-03-06 11:22:33"),
+(2 ,10 ,"2022-08-05 08:05:17"),
+(2 ,7	,"2020-01-02 07:40:33"),
+(3 ,10 ,"2020-11-13 16:55:13"),
+(3 ,2	,"2020-12-05 18:38:30"),
+(4 ,8	,"2021-08-15 17:10:10"),
+(5 ,8	,"2022-01-09 01:44:33"),
+(5 ,5	,"2020-08-06 15:23:43"),
+(6 ,7	,"2017-01-24 00:31:17"),
+(6 ,1	,"2017-10-12 12:35:20"),
+(7 ,4	,"2011-12-15 22:30:49"),
+(8 ,4	,"2012-03-17 14:56:41"),
+(9 ,9	,"2022-02-24 21:14:22"),
+(10	,3	,"2015-12-13 08:30:22");
